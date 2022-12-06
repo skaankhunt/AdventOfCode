eb598be3fef6b399fd326792b1055c09c5aaa4f7
skaankhunt <Erik.soderstrom00@gmail.com>
2022-12-06 03:07:40 +0100

    1st Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+f = open("input.txt", "r")
+input = f.read().splitlines()
+f.close()
+
+blankspaces = 0
+for x in input:
+    if x == '':
+        blankspaces += 1
+
+calories = [0] * (blankspaces + 1)
+elfcount = 0
+
+for i in input:
+    if i == '':
+        elfcount += 1
+    else:
+        calories[elfcount] += int(i)
+
+print("The elf carrying the most calories is carrying:")
+print(max(calories))
+print("\n")
+
+sum = 0
+for z in range(0,3):
+    sum += max(calories)
+    calories[calories.index(max(calories))] = 0
+    print(sum)
+
+
+
+
+
